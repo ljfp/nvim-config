394a32e093126f57b9cccf1154472a4e838b15b8
LJFP <ljfp@ljfp.xyz>
2024-10-25 17:09:20 +0100

    LazyVim configuration.

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = function()
+		return {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+		}
+	end,
+}
