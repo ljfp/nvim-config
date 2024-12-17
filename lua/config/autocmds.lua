8dd798da073fc3ef79c3b2d5ac36b4520d2c6ffa
Lautaro Javier Fernandez Pricco <ljfp@ljfp.xyz>
2024-12-17 06:54:54 +0000

    C++ source and header files will use tab instead of spaces (expandtab = false).

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set tab width and use tabs instead of spaces for C++ source and header files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "cpp", "h", "hpp" },
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.expandtab = false
+	end,
+})
